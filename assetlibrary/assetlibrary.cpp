f8cd155361bfe04a07acb27b5672d2ac3fedf8db
00steve <stevemkrenz@gmail.com>
2016-10-23 14:38:28 -0500

    Updated LoadEngineControl to be a shell for the generic Load<T> function

--- assetlibrary/assetlibrary.cpp
+++ assetlibrary/assetlibrary.cpp
@@ -44,12 +44,10 @@ T AssetLibrary::Load(Node* loader,string fileName,VarMap settings){
     }
     string funcName = string("Build");
 	T object = GetDllValue<T>(dll,funcName.c_str());
-	//pass along the global references from the parent
 	object->GlobalsRef(Node::GlobalsRef());
 	object->GlobalRequestsRef(loader->GlobalRequestsRef());
 	object->AssetLibraryRef(loader->AssetLibraryRef());
 	object->TimeStepRef(loader->TimeStepRef());
-    //loader->CopyGlobalRefsTo(object);
     object->Settings(settings);
 	return object;
 }
@@ -61,38 +59,14 @@ Node* AssetLibrary::LoadCustom(Node* loader,string settingsName,VarMap settings)
 	return Load<Node*>(loader,fileName,settings);
 }
 
-
-
-
-
 EngineControl* AssetLibrary::LoadEngineControl(Node* loader,VarMap settings){
-	string type = settings.get<string>("type");
-	string fileName = rootDirectory + "extensions/engine.control." + type + ".dll";
-	eDLL* dll = NULL;
-	if(loadedFiles.isSet(fileName)){
-		dll = (eDLL*)loadedFiles.getLastCheck();
-	} else {
-		dll = new eDLL(fileName.c_str());
-		if(!dll->IsLoaded()){
-			delete dll;
-			cout << " - couldn't load the dll for some reason\n";
-			return NULL;
-		}
-		loadedFiles.push((void*)dll,fileName);
-	}
-    string funcName = string("BuildEngineControl");
-	EngineControl* ec = GetDllValue<EngineControl*>(dll,funcName.c_str());
-	ec->GlobalsRef(loader->GlobalsRef());
-	ec->GlobalRequestsRef(loader->GlobalRequestsRef());
-	ec->AssetLibraryRef(loader->AssetLibraryRef());
-	ec->TimeStepRef(loader->TimeStepRef());
-	//loader->CopyGlobalRefsTo((Node*)ec);
-	//cout << " - loaded engine control asset library ref " << loader->AssetLibraryRef() << " from " << &ec->Assets() << endl;
-	//cout << " - loaded engine control [" << type << "] with ref : " << loader->AssetLibraryRef() << endl;
-	ec->Settings(settings);
-	return ec;
+    if(!settings.IsSet("type")){
+        cout << "invalid settings argument. No \"type\" set.\n";
+        return NULL;
+    }
+    string fileName = rootDirectory + "extensions/engine.control." + settings.get<string>("type") + ".dll";
+	return Load<EngineControl*>(loader,fileName,settings);
 }
-
 
 View* AssetLibrary::LoadView(Node* loader,VarMap settings){
     if(!settings.IsSet("type")){
@@ -102,9 +76,6 @@ View* AssetLibrary::LoadView(Node* loader,VarMap settings){
     string fileName = rootDirectory + "extensions/engine.view." + settings.get<string>("type") + ".dll";
     return Load<View*>(loader,fileName,settings);
 }
-
-
-
 
 Camera* AssetLibrary::LoadCamera(Node* loader,VarMap settings){
     if(!settings.IsSet("type")){
